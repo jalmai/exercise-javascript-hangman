54bb5e6e63979bf58ea2e4228afb52dad29cb173
mingman <ingman.m@gmail.com>
2024-02-03 15:58:34 +0100

    Refresh hangman

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-let hangmanParts = ["ground", "head", "body", "arms", "legs", "scaffold"];
+let hangmanPartsArr = ["ground", "head", "body", "arms", "legs", "scaffold"];
+let hangmanParts = [];
+let round = 0;
+let gameOver = true;
+
+class hangmanPart {
+  constructor(element, display, index) {
+    this.element = element;
+    this.display = display;
+    this.index = index;
+  }
+}
 
 function endGame() {
-  hangmanParts.forEach((e) => {
+  hangmanPartsArr.forEach((e) => {
     let part = document.getElementById(e);
     part.setAttribute("opacity", 1);
     console.log("display hangman part: " + e);
   });
 }
 function startGame() {
-  hangmanParts.forEach((e) => {
+  hangmanPartsArr.forEach((e, index) => {
     let part = document.getElementById(e);
-    part.setAttribute("opacity", 0);
-    console.log("hiding bodypart: " + e);
+    let a = new hangmanPart(part, false, index);
+    hangmanParts.push(a);
+  });
+}
+function refreshHangman() {
+  hangmanParts.forEach((e) => {
+    if (e.display) {
+      e.element.setAttribute("opacity", 1);
+    } else {
+      e.element.setAttribute("opacity", 0);
+    }
   });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   startGame();
+  refreshHangman();
 });
